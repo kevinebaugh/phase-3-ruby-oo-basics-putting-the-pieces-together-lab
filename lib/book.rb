f275a5dce381d40526c93d46daafb4300ca8d524
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-02-03 21:20:22 -0500

    commit tree 90b73acf02f0d5ca822ee42bfa7e4e3672e5f4a6

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title)
+    @title = title
+  end
 
+  def author=(name)
+    @author = name
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
